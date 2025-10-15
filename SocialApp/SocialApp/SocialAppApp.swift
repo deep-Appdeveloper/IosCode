d29defc4b613040e0dda00d1567711df4d174514
deep-Appdeveloper <sainideep.appdeveloper@gmail.com>
2025-10-15 10:11:09 +0530

    code push .

---
+++ SocialApp/SocialApp/SocialAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  SocialAppApp.swift
+//  SocialApp
+//
+//  Created by deepak saini on 15/10/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SocialAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
